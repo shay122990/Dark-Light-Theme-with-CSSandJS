856ca87600fcd270f0b27c1d256cdb37aaead159
Shay.Asanova <shay.asanova@gmail.com>
2021-07-13 22:10:08 +0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const toggle = document.querySelector('.toggle')
+
+
+
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+    if (html.classList.contains('dark')) {
+        html.classList.remove('dark')
+        e.target.innerHTML = 'Dark mode'
+    } else {
+        html.classList.add('dark')
+        e.target.innerHTML = 'Light mode'
+    }
+})
+
